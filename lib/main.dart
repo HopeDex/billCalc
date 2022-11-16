22d299c508ffe7903c9a2b2b82f6fa1ccb02de5d
Hope Dexter Madziakapita <48337899+HopeDex@users.noreply.github.com>
2022-11-16 13:23:01 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:BillCalc/pages/main_page.dart';
+
 import '../helpers/ColorsSys.dart';
 import '../helpers/Strings.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomePage(),
   ));
 }
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -20,9 +24,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    _pageController = PageController(
-      initialPage: 0
-    );
+    _pageController = PageController(initialPage: 0);
     super.initState();
   }
 
@@ -41,12 +43,14 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.white,
         actions: <Widget>[
           Padding(
-            padding: EdgeInsets.only(right: 20, top: 20),
-            child: Text('Skip', style: TextStyle(
-              color: ColorSys.gray,
-              fontSize: 18,
-              fontWeight: FontWeight.w400
-            ),),
+            padding: const EdgeInsets.only(right: 20, top: 20),
+            child: Text(
+              'Skip',
+              style: TextStyle(
+                  color: ColorSys.gray,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400),
+            ),
           )
         ],
       ),
@@ -62,28 +66,45 @@ class _HomePageState extends State<HomePage> {
             controller: _pageController,
             children: <Widget>[
               makePage(
-                image: 'assets/images/step-one.png',
-                title: Strings.stepOneTitle,
-                content: Strings.stepOneContent
-              ),
+                  image: 'assets/images/step-one.png',
+                  title: Strings.stepOneTitle,
+                  content: Strings.stepOneContent),
               makePage(
-                reverse: true,
-                image: 'assets/images/step-two.png',
-                title: Strings.stepTwoTitle,
-                content: Strings.stepTwoContent
-              ),
+                  reverse: true,
+                  image: 'assets/images/step-two.png',
+                  title: Strings.stepTwoTitle,
+                  content: Strings.stepTwoContent),
               makePage(
-                image: 'assets/images/step-three.png',
-                title: Strings.stepThreeTitle,
-                content: Strings.stepThreeContent
-              ),
+                  image: 'assets/images/step-three.png',
+                  title: Strings.stepThreeTitle,
+                  content: Strings.stepThreeContent),
             ],
           ),
           Container(
-            margin: EdgeInsets.only(bottom: 60),
+            margin: const EdgeInsets.only(bottom: 100),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: _buildIndicator(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => MainPage()));
+              },
+              child: Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width * 0.7,
+                color: Colors.blue,
+                child: Center(
+                  child: Text(
+                    "Get Started",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              ),
             ),
           )
         ],
@@ -93,41 +114,54 @@ class _HomePageState extends State<HomePage> {
 
   Widget makePage({image, title, content, reverse = false}) {
     return Container(
-      padding: EdgeInsets.only(left: 50, right: 50, bottom: 60),
+      padding: const EdgeInsets.only(left: 50, right: 50, bottom: 60),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          !reverse ? 
-          Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Image.asset(image),
-              ),
-              SizedBox(height: 30,),
-            ],
-          ) : SizedBox(),
-          Text(title, style: TextStyle(
-            color: ColorSys.primary,
-            fontSize: 30,
-            fontWeight: FontWeight.bold
-          ),),
-          SizedBox(height: 20,),
-          Text(content, textAlign: TextAlign.center, style: TextStyle(
-            color: ColorSys.gray,
-            fontSize: 20,
-            fontWeight: FontWeight.w400
-          ),),
-          reverse ? 
-          Column(
-            children: <Widget>[
-              SizedBox(height: 30,),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Image.asset(image),
-              ),
-            ],
-          ) : SizedBox(),
+          !reverse
+              ? Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Image.asset(image),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                  ],
+                )
+              : const SizedBox(),
+          Text(
+            title,
+            style: TextStyle(
+                color: ColorSys.primary,
+                fontSize: 30,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            content,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: ColorSys.gray,
+                fontSize: 20,
+                fontWeight: FontWeight.w400),
+          ),
+          reverse
+              ? Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Image.asset(image),
+                    ),
+                  ],
+                )
+              : const SizedBox(),
         ],
       ),
     );
@@ -135,20 +169,18 @@ class _HomePageState extends State<HomePage> {
 
   Widget _indicator(bool isActive) {
     return AnimatedContainer(
-      duration: Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 300),
       height: 6,
       width: isActive ? 30 : 6,
-      margin: EdgeInsets.only(right: 5),
+      margin: const EdgeInsets.only(right: 5),
       decoration: BoxDecoration(
-        color: ColorSys.secoundry,
-        borderRadius: BorderRadius.circular(5)
-      ),
+          color: ColorSys.secoundry, borderRadius: BorderRadius.circular(5)),
     );
   }
 
   List<Widget> _buildIndicator() {
     List<Widget> indicators = [];
-    for (int i = 0; i<3; i++) {
+    for (int i = 0; i < 3; i++) {
       if (currentIndex == i) {
         indicators.add(_indicator(true));
       } else {
